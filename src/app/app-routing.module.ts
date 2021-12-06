6cc22c82a9505c9a1594e0634559f3aceaa0df80
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2021-12-06 16:52:43 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent} from './about/about.component';
+import { SkillsComponent} from './skills/skills.component';
+import { ContactComponent} from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'skills', component: SkillsComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+  SERVICE_ID = 'service_r0b5wqi'
+  TEMPLATE_ID = 'template_mzog1g8'
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
